6172271563810e04a8e4c7ea9d9396aaad7d5073
Raghu Burugula <rburugula83@gmail.com>
2016-08-13 23:57:47 +0530

    Update cacheMatrix.R for ProgrammingAssignment2

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,21 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        cachmat <- NULL
+        
+        setmat <- function(ymat = matrix()) {
+                x <<- ymat
+                cachmat <<- NULL
+        }
+        
+        getmat <- function() x
+        setmatinv <- function(matinv) cachmat <<- matinv
+        getmatinv <- function() cachmat
+        
+        list(set = setmat, get = getmat,
+             setinv = setmatinv,
+             getinv = getmatinv)
+        
 }
 
 
@@ -12,4 +26,14 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        inv <- x$getinv()
+        if(!is.null(inv)) {
+                message('Retrieving matrix inverse from Cache')
+                inv
+        }
+        
+        indata <- x$get()
+        invmat <- solve(indata)
+        x$setinv(invmat)
+        invmat
 }
